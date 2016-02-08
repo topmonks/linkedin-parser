a12265b609558a3a1eea25e02d49cd933e282b84
Aleš Roubíček <ales@roubicek.name>
2016-02-08 12:09:54 +0100

    updated deps

--- project.clj
+++ project.clj
@@ -7,14 +7,14 @@
   :main linkedin-parser.web
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [enlive "1.1.6"]
-                 [environ "1.0.1"]
+                 [environ "1.0.2"]
                  [org.immutant/web "2.1.2"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring-middleware-format "0.7.0"]
                  [org.apache.pdfbox/pdfbox "1.8.10"]]
   :java-source-paths ["src"]
-  :plugins [[lein-environ "1.0.1"]]
+  :plugins [[lein-environ "1.0.2"]]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
                        :omit-source true}})
